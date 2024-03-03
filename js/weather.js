a6a668fc8258f2518f378385eaa8fcc6d2a83ec4
SOEUI KIM <58338918+DDoeuiGongju@users.noreply.github.com>
2024-03-04 03:12:18 +0900

    Add files via upload

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+const city = document.querySelector("#weather span:first-child");
+const weather = document.querySelector("#weather span:last-child");
+
+const API_KEY = "f98532f32fd5fd8ff7fe91381b1449cf";
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+        city.innerText = data.name;
+        weather.innerText = `  ${data.weather[0].main} / ${data.main.temp}º`;
+        });
+}
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
